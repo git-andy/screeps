7a968d8d87a3ddcdf829cfa8b8b578d484ed249a
git-andy <git@jenkinsonline.co.uk>
2018-08-15 10:16:49 +0100

    reorder of room config

--- config.room.js
+++ config.room.js
@@ -4,110 +4,6 @@ var config = {
     },
     "ROOM_MAP": [
         {
-            "name": "E6N42",
-            "action": "inhabit",
-            "locations": {
-                "controller": "5,30",
-                "spawn": [
-                    {
-                        "build": false,
-                        "name": "E6N42_SPAWN_1",
-                        "position": "23,21"
-                    },
-                    {
-                        "build": false,
-                        "name": "E6N42_SPAWN_2",
-                        "position": "x,y"
-                    }
-                ],
-                "link": [
-                    {
-                        "build": false,
-                        "position": "x,y"
-                    }
-                ],
-                "road": [
-                    {
-                        "build": false,
-                        "position": "x,y"
-                    }
-                ],
-                "constructedWall": [
-                    {
-                        "build": false,
-                        "position": "x,y"
-                    }
-                ],
-                "rampart": [
-                    {
-                        "build": false,
-                        "position": "x,y"
-                    }
-                ],
-                "storage": [
-                    {
-                        "build": false,
-                        "position": "x,y"
-                    }
-                ],
-                "tower": [
-                    {
-                        "build": true,
-                        "position": "22,19"
-                    }
-                ],
-                "observer": [
-                    {
-                        "build": false,
-                        "position": "x,y"
-                    }
-                ],
-                "powerSpawn": [
-                    {
-                        "build": false,
-                        "position": "x,y"
-                    }
-                ],
-                "extractor": [
-                    {
-                        "build": false,
-                        "position": "x,y"
-                    }
-                ],
-                "terminal": [
-                    {
-                        "build": false,
-                        "position": "x,y"
-                    }
-                ],
-                "lab": [
-                    {
-                        "build": false,
-                        "position": "x,y"
-                    }
-                ],
-                "container": [
-                    {
-                        "build": false,
-                        "position": "x,y"
-                    }
-                ],
-                "nuker": [
-                    {
-                        "build": false,
-                        "position": "x,y"
-                    }
-                ]
-            },
-            "resources": {
-                "energy": [
-                    {
-                        "position": "30,19"
-                    }
-                ]
-            }
-        },
-        {
             "name": "E7N42",
             "action": "inhabit",
             "locations": {
@@ -222,6 +118,110 @@ var config = {
                     },
                     {
                         "position": "34,4"
+                    }
+                ]
+            }
+        },
+        {
+            "name": "E6N42",
+            "action": "inhabit",
+            "locations": {
+                "controller": "5,30",
+                "spawn": [
+                    {
+                        "build": false,
+                        "name": "E6N42_SPAWN_1",
+                        "position": "23,21"
+                    },
+                    {
+                        "build": false,
+                        "name": "E6N42_SPAWN_2",
+                        "position": "x,y"
+                    }
+                ],
+                "link": [
+                    {
+                        "build": false,
+                        "position": "x,y"
+                    }
+                ],
+                "road": [
+                    {
+                        "build": false,
+                        "position": "x,y"
+                    }
+                ],
+                "constructedWall": [
+                    {
+                        "build": false,
+                        "position": "x,y"
+                    }
+                ],
+                "rampart": [
+                    {
+                        "build": false,
+                        "position": "x,y"
+                    }
+                ],
+                "storage": [
+                    {
+                        "build": false,
+                        "position": "x,y"
+                    }
+                ],
+                "tower": [
+                    {
+                        "build": true,
+                        "position": "22,19"
+                    }
+                ],
+                "observer": [
+                    {
+                        "build": false,
+                        "position": "x,y"
+                    }
+                ],
+                "powerSpawn": [
+                    {
+                        "build": false,
+                        "position": "x,y"
+                    }
+                ],
+                "extractor": [
+                    {
+                        "build": false,
+                        "position": "x,y"
+                    }
+                ],
+                "terminal": [
+                    {
+                        "build": false,
+                        "position": "x,y"
+                    }
+                ],
+                "lab": [
+                    {
+                        "build": false,
+                        "position": "x,y"
+                    }
+                ],
+                "container": [
+                    {
+                        "build": false,
+                        "position": "x,y"
+                    }
+                ],
+                "nuker": [
+                    {
+                        "build": false,
+                        "position": "x,y"
+                    }
+                ]
+            },
+            "resources": {
+                "energy": [
+                    {
+                        "position": "30,19"
                     }
                 ]
             }
